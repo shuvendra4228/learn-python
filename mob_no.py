3c9cc4990e907b8b350e054edeb27a3a7ef9173d
Soumitri <biki636@gmail.com>
2018-01-07 23:06:10 +0530

    mob_no.py message correction

--- mob_no.py
+++ mob_no.py
@@ -1,10 +1,9 @@
 mob_input = input("enter your mobile number:")
 if len(mob_input) == 10 and mob_input[0] == '9':
-    print("mobile is valid")
+    print("mobile number is valid")
 elif len(mob_input) == 11 and mob_input[0] == '0':
-    print("mobile number is a valid number")
+    print("mobile number is valid")
 elif len(mob_input) == 13 and mob_input[0:3] == '+91':
-    print("valid mobile number")
+    print("mobile number is valid")
 else:
-    print("invalid number")
-
+    print("invalid mobile number")
