16c52db54ec79fdcc0d044dc2edf174f4ff97f55
Shuvendra <shuvendra4227@gmail.com>
2018-01-11 21:59:04 +0530

    reverse of a number

---
+++ A1_reverse_int.py
@@ -0,0 +1,10 @@
+temp = int(input("enter a number:"))
+temp1 = temp
+rev = 0
+while temp1 > 0:
+    reminder = temp1 % 10
+    temp1 = temp1//10 
+    rev = rev*10 + reminder
+print(f"reverse of {temp} is {rev}")
+
+
