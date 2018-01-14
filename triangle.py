cb2b29998e3a9f069a2ecba705d3f35a17408131
Shuvendra <shuvendra4227@gmail.com>
2018-01-14 21:37:01 +0530

    triangle

--- triangle.py
+++ triangle.py
@@ -1,6 +1,6 @@
 input_row = int(input("enter number of rows: "))
 num = 0
-for i in range(input_row+1, 0, -1):
+for i in range(input_row, 0, -1):
     print(" "*num,end =" ")
     print("* "*i)
     num = num +1
